eb18f800719693d7e5814afd8954a335f6e5eacc
dgod1028 <dgod1028@gmail.com>
2015-12-18 06:33:29 +0900

    Update naiveClassifier.R

--- naiveClassifier.R
+++ naiveClassifier.R
@@ -15,7 +15,6 @@ Voc = function(x){
 score <- function(mail,cate){
   mail <- unlist(lapply(mail, function(x) { str_split(x, " ") }))
   mail <- subset(mail,mail != "")
-  mail = tolower(mail)
   score = prior[cate]
   if(cate == 1){voc = spam.words}
   if(cate == 2){voc = nonspam.words}
